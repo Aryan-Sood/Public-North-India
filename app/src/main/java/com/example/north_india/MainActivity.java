a16624f3f1b1c3cb239598c5ad63b5c684e56fc4
Aryan <aryansood12@gmail.com>
2023-07-31 11:45:09 +0530

    home page

--- app/src/main/java/com/example/north_india/MainActivity.java
+++ app/src/main/java/com/example/north_india/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.north_india;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        },2000);
 
 
     }
